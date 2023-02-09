c2dafacc157f12128db561cafc46d425553f757f
Sabrina Brown <sabrinaebr@gmail.com>
2023-02-09 15:52:03 -0500

    vending

--- src/main/java/com/techelevator/Menu.java
+++ src/main/java/com/techelevator/Menu.java
@@ -2,23 +2,10 @@ package com.techelevator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Menu {
 
-
-    public String setupInventory() {
-        File inventoryFile = new File("vendingmachine.csv");
-        try (Scanner textInput = new Scanner(inventoryFile)) {
-            while(textInput.hasNextLine()) {
-                String lineOfInput = textInput.nextLine();
-                String[] itemInfoArray = lineOfInput.split("|");
-                Item "(itemInfoArray[0])" = new item;
-            }
-        }
-                catch (FileNotFoundException e) {
-        }
-
-    }
-
 }
+
--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -2,15 +2,35 @@ package com.techelevator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class VendingMachineCLI {
 
 	public static void main(String[] args) {
+
+
 		File inventoryFile = new File("vendingmachine.csv");
+		int count = 0;
+		Map<Integer, Item> inventory = new HashMap<Integer, Item>();
 		try (Scanner textInput = new Scanner(inventoryFile)) {
+			while (textInput.hasNextLine()) {
+				count++;
+
+				String lineOfInput = textInput.nextLine();
+				String[] itemInfoArray = lineOfInput.split("\\|");
+				BigDecimal stringToDec = new BigDecimal(itemInfoArray[2]);
+				Item a = new Item(itemInfoArray[0], itemInfoArray[1], stringToDec, itemInfoArray[3]);
+				inventory.put(count, a);
+			}
 		} catch (FileNotFoundException e) {
+		}
+		for (Map.Entry<Integer, Item> element : inventory.entrySet()) {
+			System.out.println(element.getValue().getName());
 		}
 
 	}
+
 }
