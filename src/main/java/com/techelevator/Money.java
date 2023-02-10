dcac60d2d1bddfe22aefc3127d0b026f56a361e1
Corey Cargill <ccarg22@gmail.com>
2023-02-10 12:51:45 -0500

    Money Class Finished

--- src/main/java/com/techelevator/Money.java
+++ src/main/java/com/techelevator/Money.java
@@ -18,18 +18,48 @@ public class Money {
 
 
     public void feedMoney(BigDecimal dollars) {
-        balance.add(dollars);
+        balance=balance.add(dollars);
+
     }
 
     public Item purchase(String userInput, Map<Integer, Item> inventoryMap) {
         for (Map.Entry<Integer, Item> element : inventoryMap.entrySet()) {
             if (userInput.toUpperCase().equals(element.getValue().getSlot()) && balance.compareTo(element.getValue().getPrice()) != -1) {
-                balance.subtract(element.getValue().getPrice());
-                Item selectedItem = element.getValue();
-                return selectedItem;
+                if(element.getValue().getQuantity()==0){
+                    System.out.println("SOLD OUT");
+                }
+                else {
+                    balance=balance.subtract(element.getValue().getPrice());
+                    Item selectedItem = element.getValue();
+                    element.getValue().setQuantity(element.getValue().getQuantity()-1);
+                    System.out.println(element.getValue().getQuantity());
+                    return selectedItem;
+                }
             }
+            else if (userInput.toUpperCase().equals(element.getValue().getSlot()) && balance.compareTo(element.getValue().getPrice()) == -1) {
+                System.out.println("Insufficient Funds");
             }
-        return null;
         }
+        return null;
+    }
+    public String correctChange(){
+        int quarterCount = 0;
+        int dimeCount=0;
+        int nickelCount=0;
+
+        while(balance.compareTo(quarter) != -1){
+            quarterCount++;
+            balance=balance.subtract(quarter);
+        }
+        while(balance.compareTo(dime) != -1){
+            dimeCount++;
+            balance=balance.subtract(dime);
+        }
+        while(balance.compareTo(nickel) != -1){
+            nickelCount++;
+            balance=balance.subtract(nickel);
+        }
+        return "Change Due: "+ "\nQuarters: "+ quarterCount+"\n Dimes: "+ dimeCount+ "\n Nickels: " + nickelCount ;
 
     }
+}
--- src/main/java/com/techelevator/PurchaseMenu.java
+++ src/main/java/com/techelevator/PurchaseMenu.java
@@ -13,23 +13,31 @@ public class PurchaseMenu {
         boolean validEntry = false;
         while (validEntry == false) {
             System.out.println();
-            System.out.println("Current Money Provided: ");
+            System.out.println("Current Money Provided: "+ money.getBalance());
             System.out.println();
             System.out.println("(1) Feed Money");
             System.out.println("(2) Select Product");
             System.out.println("(3) Finish Transaction");
             String option = userInput.nextLine();
             if (option.equals("1")) {
-                // Feed Money
-//                System.out.println();
-//                System.out.println("Current money provided: $" + money.getBalance());
-//                System.out.println("Please enter bills.");
-//                String bills = userInput.nextLine();
-//                BigDecimal stringToDec = new BigDecimal(bills);
-//                money.feedMoney(stringToDec);
-                validEntry = true;
+                System.out.println();
+                System.out.println("Current money provided: $" + money.getBalance());
+                System.out.println();
+                System.out.println("Please enter bills. Or 0 to exit.");
+
+                String bills = userInput.nextLine();
+                BigDecimal stringToDec = new BigDecimal(bills);
+                money.feedMoney(stringToDec);
 
             } else if (option.equals("2")) {
+                for (Map.Entry<Integer, Item> element : inventory.entrySet()) {
+                    if (element.getValue().getQuantity() == 0) {
+                        System.out.println(element.getValue().getSlot() + " " + element.getValue().getName() + " " + element.getValue().getPrice() + " SOLD OUT");
+
+                    } else
+                        System.out.println(element.getValue().getSlot() + " " + element.getValue().getName() + " Price: " + element.getValue().getPrice() + " Quantity: " + element.getValue().getQuantity());
+                }
+                System.out.println();
                 System.out.println("Please enter slot ID or 0 to exit: ");
                 String slotID = userInput.nextLine();
                 if (!slotID.equals("0")) {
@@ -52,6 +60,8 @@ public class PurchaseMenu {
             }
             else if (option.equals("3")) {
                 // Finish
+                String totalChange = money.correctChange();
+                System.out.println(totalChange);
                 validEntry = true;
 
             } else {
